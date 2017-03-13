f96eea713f06dd8e97dbf391ecc6ed4c0f0ce5ee
wxcican <wxcican@qq.com>
2017-03-13 14:36:12 +0800

    实现Vitamio中MediaPlayer+SurfaceView播放视频

--- app/src/main/java/com/fuicuiedu/xc/video_demo_20170310/DemoB/DemoBActivity.java
+++ app/src/main/java/com/fuicuiedu/xc/video_demo_20170310/DemoB/DemoBActivity.java
@@ -2,8 +2,11 @@ package com.fuicuiedu.xc.video_demo_20170310.DemoB;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.fuicuiedu.xc.video_demo_20170310.R;
+import com.fuicuiedu.xc.video_demo_20170310.VideoUrlRes;
 
 
 public class DemoBActivity extends AppCompatActivity {
@@ -12,5 +15,18 @@ public class DemoBActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo_b);
+
+        //拿到VideoView
+        VideoView videoView = (VideoView) findViewById(R.id.demo_b_vv);
+        //设置数据源
+        videoView.setVideoPath(VideoUrlRes.getTestVideo1());
+        //开始播放
+        videoView.start();
+
+        //实例化控制器
+        MediaController mediaController = new MediaController(this);
+        //添加控制器
+        videoView.setMediaController(mediaController);
+
     }
 }
--- app/src/main/java/com/fuicuiedu/xc/video_demo_20170310/DemoC/DemoCActivity.java
+++ app/src/main/java/com/fuicuiedu/xc/video_demo_20170310/DemoC/DemoCActivity.java
@@ -1,16 +1,84 @@
 package com.fuicuiedu.xc.video_demo_20170310.DemoC;
 
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 import com.fuicuiedu.xc.video_demo_20170310.R;
+import com.fuicuiedu.xc.video_demo_20170310.VideoUrlRes;
+
+import java.io.IOException;
+
+import io.vov.vitamio.MediaPlayer;
 
 
 public class DemoCActivity extends AppCompatActivity {
+
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo_c);
+
+
+        surfaceView = (SurfaceView) findViewById(R.id.demo_c_sv);
+        surfaceHolder = surfaceView.getHolder();
+
+        //使用surfaceView时，要对PixelFormat（像素格式）处理，否则会花屏！
+        surfaceHolder.setFormat(PixelFormat.RGBA_8888);
+
+        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    mediaPlayer = new MediaPlayer(DemoCActivity.this);
+                    mediaPlayer.setDisplay(surfaceHolder);
+                    mediaPlayer.setDataSource(VideoUrlRes.getTestVideo1());
+                    mediaPlayer.prepareAsync();
+
+                    mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            mediaPlayer.start();
+                        }
+                    });
+
+                    //Vitamio5.0,需要进行Audio处理，才能对在线视频进行播放！！！
+                    mediaPlayer.setOnInfoListener(new MediaPlayer.OnInfoListener() {
+                        @Override
+                        public boolean onInfo(MediaPlayer mp, int what, int extra) {
+                            if (what == MediaPlayer.MEDIA_INFO_FILE_OPEN_OK){
+                                //进行Audio处理
+                                mediaPlayer.audioInitedOk(mediaPlayer.audioTrackInit());
+                                return true;
+                            }
+                            return false;
+                        }
+                    });
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+            }
+        });
+
+
     }
 }
